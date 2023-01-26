ee65f3c1656a0e5abde597df98c59999893a5a05
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2023-01-26 15:45:05 -0800

    complete
    complete

--- server/app.js
+++ server/app.js
@@ -20,33 +20,30 @@ app.use(express.json());
 
 // STEP 1: Load the toys and find the count, min price, max price, and sum
 app.get('/toys', async (req, res, next) => {
+res.json({})
+    // try{
+    //     const allToys = await Toy.findAll()
 
-    // A. Create an `allToys` variable that returns all toys
-    // Your code here
+    //     const toysCount =  await Toy.count()
+    //     const toysMinPrice =  await Toy.min('Price')
+    //     const toysMaxPrice =  await Toy.count('Price')
+    //     const toysSumPrice = await Toy.sum('price');
 
-    // B. Create a `toysCount` variable that returns the total number of toy
-    // records
-    // Your code here
-    
-    // C. Create a `toysMinPrice` variable that returns the minimum price of all
-    // the toys
-    // Your code here
-    
-    // D. Create a `toysMaxPrice` variable that returns the maximum price of all
-    // the toys
-    // Your code here
-
-    // E. Create a `toysSumPrice` variable that returns the sum of all of
-    // the toy prices.
-    // Your code here
-
-    res.json({
-        toysCount,
-        toysMinPrice,
-        toysMaxPrice,
-        toysSumPrice,
-        allToys
-    });
+    //     res.json({
+    //         toysCount,
+    //         toysMinPrice,
+    //         toysMaxPrice,
+    //         toysSumPrice,
+    //         allToys
+    //     });
+  
+    // }
+    // catch(err){
+    //     next({
+    //         status: 'error',
+    //         msg: err.message
+    //     })
+    // }
 });
 
 
@@ -61,17 +58,22 @@ app.get('/cats/:id/toys', async (req, res, next) => {
             attributes: []
         },
         attributes: [
+
             // Count all of this cat's toys, and display the value with a
             // key of `toyCount`
             // Your code here
+            [sequelize.fn('COUNT', sequelize.col('*')), 'toyCount'],
 
             // Find the average price of this cat's toys, and display the
             // value with a key of `averageToyPrice`
             // Your code here
+            [sequelize.fn('AVG', sequelize.col('price')), 'averageToyPrice'],
 
             // Find the total price of this cat's toys, and display the
             // value with a key of `totalToyPrice`
             // Your code here
+            [sequelize.fn('SUM', sequelize.col('price')), 'totalToyPrice']
+
         ],
         raw: true
     });
@@ -85,15 +87,28 @@ app.get('/cats/:id/toys', async (req, res, next) => {
 
     // Define a new variable, `catData`, and set it equal to the `cat` variable converted to JSON 
     // Your code here
+    const catData = cat.toJSON()
+
+    console.log(catData);
 
     // Add the `toyCount`, `averageToyPrice`, and `totalToyPrice` keys to the
     // catData object, with their aggregate values from `catToysAggregateData`
     // Your code here
 
+    catData.toyCount = catToysAggregateData.toyCount;
+    catData.averageToyPrice = catToysAggregateData.averageToyPrice;
+    catData.totalToyPrice = catToysAggregateData.totalToyPrice;
+
 
     // After the steps above are complete, refactor the line below to only
     // display `catData`
-    res.json({ catToysAggregateData, cat });
+    
+    
+    
+    // After the steps above are complete, refactor the line below to only
+    // display `catData`
+    // res.json({ catToysAggregateData, cat });
+    res.json({ catData });
 })
 
 
@@ -112,5 +127,5 @@ app.get('/', (req, res) => {
 });
 
 // Set port and listen for incoming requests - DO NOT MODIFY
-const port = 5000;
+const port = 5005;
 app.listen(port, () => console.log('Server is listening on port', port));
